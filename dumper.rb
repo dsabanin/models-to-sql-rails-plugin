036afdc7dbed4a2fcdf6ab71152ffa635411a641
Dima Sabanin <sdmitry@gmail.com>
2008-04-24 13:45:38 +0800

    More sophisticated code to traverse the graph of objects. Allows YAML fixtures generation

--- dumper.rb
+++ dumper.rb
@@ -1,72 +1,19 @@
 class Array
   
   def to_sql(*anything)
-    map do |m|
-      m.to_sql(*anything)
-    end #.compact.join(";\n")
-  end
-  
-end
-
-class TreeClimber
-  
-  def initialize(model, baton)
-    @model = model
-    @baton = baton
-    initialize_baton
-  end
-  
-  def initialize_baton
+    bananas = map do |leaf|
+      leaf.to_sql(*anything)
+    end
+    SQLMonkey.sort_out_bananas(bananas)
   end
   
 end
 
 module ActiveRecord
   class Base
-    
-    def convert_itself_to_insert_sql
-      quoted_attributes = attributes_with_quotes
-      "INSERT INTO #{self.class.quoted_table_name} " +
-      "(#{quoted_column_names.join(', ')}) " +
-      "VALUES(#{quoted_attributes.values.join(', ')})"
-    end
-    
-    def convert_itself_to_delete_sql
-      "DELETE FROM #{self.class.quoted_table_name} WHERE #{connection.quote_column_name(self.class.primary_key)} = #{quoted_id}"
-    end
-    
+
     def to_sql(baton={})
-      baton[:ignore_associations_for] ||= []
-      baton[:ignore_models] ||= []
-      baton[:ignore_tables] ||= []
-      baton[:dumped_ids] ||= Hash.new { |hsh,key| hsh[key] = Array.new }
-      baton[:current_level] ||= baton[:level].to_i
-      baton[:debug] ||= false
-      baton[:add_deletes] ||= false
-      
-      return if baton[:ignore_models].include?(self.class)
-      return if baton[:ignore_tables].include?(self.class.table_name)
-      
-      if baton[:dumped_ids][self.class].include?(id)
-        return
-      else
-        baton[:dumped_ids][self.class] << id
-      end
-      dumped_sql = []      
-      dumped_sql << convert_itself_to_delete_sql if baton[:add_deletes]
-      dumped_sql << convert_itself_to_insert_sql
-      STDERR << "Dumping #{self.class}:#{id}\n" if baton[:debug]
-      
-     if baton[:level]
-        baton[:current_level] -= 1
-      end
-      if !baton[:ignore_associations_for].include?(self.class) and baton[:current_level] >= 0
-        self.class.reflect_on_all_associations.each do |assoc|
-          assoc_value = self.send(assoc.name)
-          dumped_sql << assoc_value.to_sql(baton) if assoc_value
-        end
-      end
-      dumped_sql.flatten.compact.join(";\n")
+      TreeClimber.new(self, baton).climb_with(SQLMonkey)
     end
   
     def to_sql_file(filepath, options={})
@@ -76,4 +23,98 @@ module ActiveRecord
     end
     
   end
+end
+
+module SQLMonkey
+  
+  extend self
+  
+  def harvest(model, baton)
+    bananas = []
+    if baton[:add_deletes]
+      bananas << "DELETE FROM #{model.class.quoted_table_name} WHERE #{model.connection.quote_column_name(model.class.primary_key)} = #{model.quoted_id}"
+    end
+    bananas << "INSERT INTO #{model.class.quoted_table_name} " +
+      "(#{model.send(:quoted_column_names).join(', ')}) " +
+      "VALUES(#{model.send(:attributes_with_quotes).values.join(', ')})"
+    bananas
+  end
+  
+  def sort_out_bananas(bananas)
+    bananas.join(";\n")
+  end
+  
+end
+
+module FixtureMonkey
+  
+  extend self
+  
+  def harvest(model, bacon)
+    { "#{model.class.to_s.tableize}_#{model.id}" => model.attributes }.to_yaml(:separator => "")
+  end
+  
+  def sort_out_bananas(bananas)
+    bananas.join("\n")
+  end
+  
+end
+
+class TreeClimber
+  
+  attr_reader :model, :baton
+  
+  def initialize(model, baton={})
+    @model = model
+    @baton = baton
+    initialize_baton
+  end
+  
+  def initialize_baton
+    baton[:ignore_associations_for] ||= []
+    baton[:ignore_models] ||= []
+    baton[:ignore_tables] ||= []
+    baton[:dumped_ids] ||= Hash.new { |hsh,key| hsh[key] = Array.new }
+    baton[:current_level] ||= baton[:level].to_i
+    baton[:debug] ||= false
+    baton[:add_deletes] ||= false    
+  end
+  
+  def climb_with(monkey)    
+    return if baton[:ignore_models].include?(model.class)
+    return if baton[:ignore_tables].include?(model.class.table_name)
+    
+    if baton[:dumped_ids][model.class].include?(model.id)
+      return
+    else
+      baton[:dumped_ids][model.class] << model.id
+    end
+    
+    bananas = []      
+    bananas << monkey.harvest(model, baton)
+    STDERR << "Getting banana #{model.class}:#{model.id}\n" if baton[:debug]
+    
+    if baton[:level]
+      baton[:current_level] -= 1
+    end
+    
+    if !baton[:ignore_associations_for].include?(model.class) and baton[:current_level] >= 0
+      model.class.reflect_on_all_associations.each do |assoc|
+        assoc_value = model.send(assoc.name)
+        if assoc_value
+          unless assoc_value.is_a? Array
+            leafs = [ assoc_value ]
+          else
+            leafs = assoc_value
+          end
+          leafs.each do |leaf|          
+            bananas << TreeClimber.new(leaf, baton).climb_with(monkey)
+          end
+        end
+      end
+    end
+    
+    monkey.sort_out_bananas(bananas.flatten.compact)
+  end
+  
 end
